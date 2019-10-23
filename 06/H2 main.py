ee4dcd7181f2096099dd9dc081e37db534316a42
JZ <jacobzhu@JZs-MacBook-Pro.local>
2019-10-23 23:26:18 +0800

    updatedddd

--- 06/H2 main.py
+++ 06/H2 main.py
@@ -1,20 +1,16 @@
 def TicTacDraw(board): 
     dict = {0:'O',1:'X',2:' '}
     n=len(board)
-    for a in range(n):
-        s=''
+    for i in range(n):
+        s=' '
         for b in range(n):
-            if board[a][b]==0:
-                s+='o'
-            elif board[a][b]==1:
-                s+='x'
-            elif board[a][b]==2:
-                s+=' '
-                if a>0:
-                    print((4*n-1)*'-')
-        str=' | '
-        s=str.join(s)
-        print(' '+s)
+            if b !=n-1:
+                s+= dict[board[i][b]]+' | '
+            else:
+                s+= dict[board[i][b]]
+        print(s)
+        if i!=n-1:
+             print((4*n-1)*'-')
 
 if __name__ == '__main__':
     board = [[0, 1, 2], [2, 0, 0], [1, 1, 2]]
